da82df1099ac1041f0a28fb4be958cf831419a82
Harshith Jain <harshith.r@listertechnologies.com>
2020-03-13 15:39:59 +0530

    Added JSDoc and little cleanup

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ var express = require('express');
 const axios = require('axios')
 
 var app = express();
+//AWS Lambda URL
 const url = `https://ltvhkgn1mj.execute-api.us-east-1.amazonaws.com/Prod/validate-phone?`
+
 app.use(express.json());
 app.use(express.static('./views'));
 app.use("/js", express.static('./js/'));
 
+/**
+ * Entry Point of Application
+ */
 app.get("/", function(req, res){
   res.render("index.html")
 })
 
+/**
+ * Exposing /validate as REST Api
+ * This will invoke AWS Lambda to validate phone number 
+ */
 app.post('/validate', function (req, res) {
   const phoneNumber = `IsoCountryCode=${req.body.countryCode}&PhoneNumber=${req.body.phoneNumber}`
   axios.get(url+phoneNumber)
@@ -22,6 +31,9 @@ app.post('/validate', function (req, res) {
   });
 });
 
+/**
+ * Port on which application is hosted locally
+ */
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
