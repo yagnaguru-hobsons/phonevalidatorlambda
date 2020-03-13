d0c8d3c7438d8db6b4ddc126b8eea2ad841ad0db
Yagnaguru Ravichandran <yagnaguru.r@hobsons.com>
2020-03-13 18:25:33 +0530

    changed url

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ var express = require('express');
 const axios = require('axios')
 
 var app = express();
-const url = `https://ltvhkgn1mj.execute-api.us-east-1.amazonaws.com/Prod/validate-phone?`
 app.use(express.json());
 app.use(express.static('./views'));
 app.use("/js", express.static('./js/'));
 
+const url = `https://ltvhkgn1mj.execute-api.us-east-1.amazonaws.com/Prod/validate-phone?`
+
+/**
+ * 
+ */
 app.get("/", function(req, res){
   res.render("index.html")
 })
 
+/**
+ * 
+ */
 app.post('/validate', function (req, res) {
   const phoneNumber = `IsoCountryCode=${req.body.countryCode}&PhoneNumber=${req.body.phoneNumber}`
   axios.get(url+phoneNumber)
